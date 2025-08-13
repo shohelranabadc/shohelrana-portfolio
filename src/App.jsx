1870f7f10ebec6b1c2ed040dd7849d68dfcf445c
Mohammad Shohel Rana <47520471+shohelranabadc@users.noreply.github.com>
2025-08-13 16:35:18 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import AdminPanel from "./components/AdminPanel"
+import LoginForm from "./components/LoginForm"
+
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="my-5 mx-3 text-amber-500 text-center text-4xl">project set up file react with tailwind css</h1>
+      <AdminPanel />
+      <LoginForm />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/AdminPanel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminPanel = () => {
+  return (
+    <div>Admin panel componet</div>
+  )
+}
+
+export default AdminPanel
